9653a20d2c635793df241c7361cc84e59b43142f
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2011-08-08 12:16:52 +0000

    Import of MANWAR/WWW-MovieReviews-NYT-0.04 from CPAN.
    gitpan-cpan-distribution: WWW-MovieReviews-NYT
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         MANWAR/WWW-MovieReviews-NYT-0.04.tar.gz
    gitpan-cpan-author:       MANWAR
    gitpan-cpan-maturity:     released

--- lib/WWW/MovieReviews/NYT.pm
+++ lib/WWW/MovieReviews/NYT.pm
@@ -14,11 +14,11 @@ WWW::MovieReviews::NYT - Interface to NewYorkTimes Movie Reviews API.
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 Readonly my $API_VERSION => 'v2';
 Readonly my $API_URL     =>
 {
